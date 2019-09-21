327f854407417da808bfdee769532d4f6495a033
jeromeschmidt <schmidt.jerome@yahoo.com>
2019-09-21 16:12:24 -0700

    initial commit

---
+++ dog.py
@@ -0,0 +1,9 @@
+# dog.py
+class Dog:
+    # Required properties are defined inside the __init__ constructor method
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+    # Methods are defined as their own named functions inside the class
+    def bark(self):
+        print("Woof!")
